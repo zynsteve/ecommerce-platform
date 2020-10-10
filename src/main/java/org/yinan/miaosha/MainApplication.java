3fca6efef3a0379826cfd7ec86e312224fef9a7b
Yinan Zhu <zynsteve@tamu.edu>
2020-10-09 22:15:49 -0500

    first commit

---
+++ src/main/java/org/yinan/miaosha/MainApplication.java
@@ -0,0 +1,11 @@
+package org.yinan.miaosha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(MainApplication.class, args);
+    }
+}
---
+++ src/main/java/org/yinan/miaosha/controller/SampleController.java
@@ -0,0 +1,15 @@
+package org.yinan.miaosha.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/demo")
+public class SampleController {
+    @RequestMapping("/thymeleaf")
+    public String thymeleaf(Model model) {
+        model.addAttribute("name", "yinan");
+        return "hello";
+    }
+}
