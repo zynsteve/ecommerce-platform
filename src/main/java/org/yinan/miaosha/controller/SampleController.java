76abc4a33f8ab17cfd9df5a63cd1ac6e226bddfe
Yinan Zhu <zynsteve@tamu.edu>
2020-10-11 14:39:22 -0500

    integrated with mybatis

--- src/main/java/org/yinan/miaosha/controller/SampleController.java
+++ src/main/java/org/yinan/miaosha/controller/SampleController.java
@@ -1,15 +1,37 @@
 package org.yinan.miaosha.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.yinan.miaosha.domain.User;
+import org.yinan.miaosha.result.Result;
+import org.yinan.miaosha.service.UserService;
 
 @Controller
 @RequestMapping("/demo")
 public class SampleController {
+    @Autowired
+    UserService userService;
+
     @RequestMapping("/thymeleaf")
     public String thymeleaf(Model model) {
         model.addAttribute("name", "yinan");
         return "hello";
+    }
+
+    @RequestMapping("/db/get")
+    @ResponseBody
+    public Result<User> dbGet() {
+        User user = userService.getById(1);
+        return Result.success(user);
+    }
+
+    @RequestMapping("/db/tx")
+    @ResponseBody
+    public Result<Boolean> dbTx() {
+        userService.tx();
+        return Result.success(true);
     }
 }
